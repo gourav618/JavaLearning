1dbd5ee86fae94c13ef4f83ae231831c51eed0e6
Gourav Sharma <sgourav533@gmail.com>
2021-01-19 17:26:16 +0530

    added file

---
+++ Learning/src/com/chapter/DemoMultipleReturn.java
@@ -0,0 +1,10 @@
+package com.chapter;
+
+public class DemoMultipleReturn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Learning/src/com/chapter/DemoParamPassingTechnique.java
+++ Learning/src/com/chapter/DemoParamPassingTechnique.java
@@ -1,9 +1,78 @@
 package com.chapter;
 
+//Call by Value 
+
+//Callee 
+class CallByValue { 
+
+ // Function to change the value 
+ // of the parameters 
+ public static void Example(int x, int y) 
+ { 
+     x++; 
+     y++; 
+ } 
+}
+
+//Call by Reference 
+
+//Callee 
+class CallByReference { 
+
+ int a, b; 
+
+ // Function to assign the value 
+ // to the class variables 
+ CallByReference(int x, int y) 
+ { 
+     a = x; 
+     b = y; 
+ } 
+
+ // Changing the values of class variables 
+ void ChangeValue(CallByReference obj) 
+ { 
+     obj.a += 10; 
+     obj.b += 20; 
+ } 
+} 
+
 public class DemoParamPassingTechnique {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+        int a = 10; 
+        int b = 20; 
+  
+        // Instance of class is created 
+        CallByValue object1 = new CallByValue(); 
+  
+        System.out.println("Value of a: " + a 
+                           + " & b: " + b); 
+  
+        // Passing variables in the class function 
+        object1.Example(a, b); 
+  
+        // Displaying values after 
+        // calling the function 
+        System.out.println("Value of a: "
+                           + a + " & b: " + b); 
+        
+        //--------------------------------------------//
+        // Instance of class is created 
+        // and value is assigned using constructor 
+        CallByReference object 
+            = new CallByReference(10, 20); 
+  
+        System.out.println("Value of a: "  + object.a   + " & b: " + object.b); 
+  
+        // Changing values in class function 
+        object.ChangeValue(object); 
+  
+        // Displaying values 
+        // after calling the function 
+        System.out.println("Value of a: " + object.a   + " & b: " + object.b);
 
 	}
 
